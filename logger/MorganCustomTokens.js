3361259d53f86f63da6a17d29e85d0fd99f355e5
dipanjal <dipanjalmaitra@gmail.com>
2020-04-02 11:14:53 +0600

    real ip added into log

--- logger/MorganCustomTokens.js
+++ logger/MorganCustomTokens.js
@@ -1,9 +1,20 @@
 const morgan = require('morgan');
+const ipware = require('ipware')();
 
 morgan.token('host', function(req, res) {
-    return req.hostname;
+    let ip_info =  ipware.get_ip(req);
+    return ipware.is_loopback_ip(ip_info.clientIp) ?
+        Private.getLoopBackIp(req) : ip_info.clientIp;
 });
 
 morgan.token('param', function(req, res, param) {
     return req.params[param];
 });
+
+let Private = {
+    getLoopBackIp : req => {
+        let lookbackIp = '127.0.0.1';
+        req['clientIp'] = lookbackIp;
+        return lookbackIp;
+    }
+}
